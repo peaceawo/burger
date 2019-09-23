ccd4fd49b4b4f8945ed246dca9fb7ee146208df3
Peace Akpai <peaceawo@gmail.com>
2019-09-22 20:20:42 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Avocado', true);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('BBQ Cheddah', true);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Truffle Mushrooms', true);
+INSERT INTO burgers  (burger_name) VALUES ('Spicy Jalapeno');
+
+SELECT * FROM burgers;
